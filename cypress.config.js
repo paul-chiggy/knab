c5ed423ca208a9b82d8ce9a17a835400c38563f1
Pavlo Chigishev <pavlo@Pavlos-MacBook-Pro.local>
2022-11-26 20:22:22 +0100

    Added mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,23 @@
 const { defineConfig } = require("cypress");
 const cucumber = require("cypress-cucumber-preprocessor").default;
+const { beforeRunHook, afterRunHook } = require("cypress-mochawesome-reporter/lib");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
     /* eslint-disable */
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on("file:preprocessor", cucumber());
+      require("cypress-mochawesome-reporter/plugin")(on);
+      on("before:run", async (details) => {
+        console.log("override before:run");
+        await beforeRunHook(details);
+      });
+      on("after:run", async () => {
+        console.log("override after:run");
+        await afterRunHook();
+      });
     },
     /* eslint-disable */
     specPattern: "**/*.feature",
