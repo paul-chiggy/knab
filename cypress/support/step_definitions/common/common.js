7ecd16b992f937943dc836b6541febe5e2c3df68
Pavlo Chigishev <pavlo@Pavlos-MacBook-Pro.local>
2022-11-27 18:53:43 +0100

    Fine-tuned api tests

--- cypress/support/step_definitions/common/common.js
+++ cypress/support/step_definitions/common/common.js
@@ -1,4 +1,4 @@
-import { Before, After } from "cypress-cucumber-preprocessor/steps";
+import { Before } from "cypress-cucumber-preprocessor/steps";
 
 // this will get called before each scenario
 Before(() => {
@@ -8,14 +8,4 @@ Before(() => {
 // this will only get called before scenarios tagged with @smoke
 Before({ tags: "@smoke" }, () => {
   cy.log("smoke before");
-});
-
-// this will get called before each scenario
-After(() => {
-  cy.log("common after");
-});
-  
-// this will only get called before scenarios tagged with @smoke
-After({ tags: "@smoke" }, () => {
-  cy.log("smoke after");
 });
--- cypress/support/step_definitions/create-board/create-board.js
+++ cypress/support/step_definitions/create-board/create-board.js
@@ -1,7 +1,10 @@
-import { Given, Then, When, And } from "cypress-cucumber-preprocessor/steps";
+import { After, Given, Then, When, And } from "cypress-cucumber-preprocessor/steps";
 
 // vars for tests
 var boardName;
+var statusOk;
+var statusBadRequest;
+var boardIds = new Array();
 
 Given("I have api key and token", () => {
   cy.log("key: " + Cypress.env("apiKey") + " | token: " + Cypress.env("token"));
@@ -18,11 +21,11 @@ When("I provide invalid input data", () => {
 });
 
 Then("API creates a valid board", () => {
-  cy.log("API retuns successful response");
+  expect(statusOk).to.eq(200);
 });
 
 Then("API returns Bad request response", () => {
-  cy.log("API retuns bad request response");
+  expect(statusBadRequest).to.eq(400);
 });
 
 And("I call createBoard endpoint", () => {
@@ -34,8 +37,33 @@ And("I call createBoard endpoint", () => {
       failOnStatusCode: false
     })
     .then(res => {
-      cy.log(res);
+      cy.log(res.status);
       console.log(res);
+      if(boardName == "") statusBadRequest = res.status;
+      else {
+        statusOk = res.status;
+        boardIds.push(res.body.id);
+      }
     });
   cy.log("I call createBoard endpoint");
 });
+
+// this will get called before each scenario
+After(() => {
+  boardIds.forEach(id => {
+    cy.request(
+      {
+        method: "DELETE",
+        url: "https://api.trello.com/1/boards/" + id + "?key=" + Cypress.env("apiKey") + "&token=" + Cypress.env("token"),
+        body: {},
+        failOnStatusCode: false
+      });
+    cy.log("Deleting board " + id);
+  });
+  boardIds.splice(0, boardIds.length);
+});
+  
+// this will only get called before scenarios tagged with @smoke
+// After({ tags: "@smoke" }, () => {
+//   cy.log("smoke after");
+// });
